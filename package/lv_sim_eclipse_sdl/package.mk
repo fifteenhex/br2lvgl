4e321bdabdf383dcd709ef62b244fe6a38bb9ea4
Daniel Palmer <daniel@0x0f.com>
2022-02-23 01:04:35 +0900

    Add sim

---
+++ package/lv_sim_eclipse_sdl/package.mk
@@ -0,0 +1,6 @@
+LV_SIM_ECLIPSE_SDL_VERSION = 5d28bcc1f347ec79d3b0c35494b5f6224ed9b20d
+LV_SIM_ECLIPSE_SDL_SITE = https://github.com/lvgl/lv_sim_eclipse_sdl.git
+LV_SIM_ECLIPSE_SDL_SITE_METHOD = git
+LV_SIM_ECLIPSE_SDL_GIT_SUBMODULES = YES
+
+$(eval $(cmake-package))
